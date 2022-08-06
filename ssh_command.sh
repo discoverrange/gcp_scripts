68be0446e379e7f89c79a22a179d25c092daeb9a
discoverrange <105451359+discoverrange@users.noreply.github.com>
2022-08-06 18:04:45 +0530

    Create ssh_command.sh

---
+++ ssh_command.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -v
+  for instance in $(gcloud compute instances list --format='value[separator=","](status,name)');  do
+    s="${instance%,*}";  #instance status
+    n="${instance#*,}";  #instance name
+    z="$(gcloud compute instances list --filter="name:$n" --format='value(zone)')"
+    zone=$z; #instance zone
+    echo "GCP"
+    echo "Instance Name:" $n >>list_instance.txt   #storing total instance list 
+    echo "Instance Name:" $n 
+    echo "Instance Status:" $s 
+    echo "Instance Zone:" $zone
+   
+      if [ $s == "RUNNING" ] #checking instance status 
+        then 
+            agent_status="$(gcloud compute ssh $n --zone $zone --command='systemctl is-active [AGENT_SERVICE]') " # add your agent service here to check the status
+            if [ $agent_status == "active" ] #checking agent status
+            then
+                printf "Agent already installed\n\n "
+            else 
+                printf "Installation started \n"
+                gcloud compute ssh $n --zone $zone --command=" COMMAND " #add your agent installation command
+                printf "Completed Agent\n\n" 
+            fi
+        else
+            printf "\n\n"
+            echo "Cortex Status: Skipped as instance is teminated:" $n >> /tmp/status.txt  #storing skipped instances
+        fi
+ done
